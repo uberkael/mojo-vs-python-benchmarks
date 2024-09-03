99df50c36ae1f41c7bb824cb840bc3b73a834314
KaeL <uberkael@gmail.com>
2024-09-03 08:00:22 +0200

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-COMPILE="mojo build *.mojo"
-BENCH="hyperfine -N -w 10 "
+BENCH="hyperfine -i -N -w 10 "
 
 for dir in *; do
-	if [ -d "$dir" ]; then
-		COMMANDS=("python ${dir}.py")
-		COMMANDS+=($dir)
-		cd "$dir"
-		echo
-		echo "Benchmarking $dir"
-		echo "======================"
-		$COMPILE
-		$BENCH "${COMMANDS[@]}"
-		cd ..
-	fi
+  if [ -d "$dir" ]; then
+    # Compile command with the name
+    COMPILE="mojo build ${dir}.mojo -o ${dir}.bin"
+    # Generate commands to run
+    COMMANDS=("python ${dir}.py")
+    COMMANDS+=("mojo ${dir}.mojo")
+    COMMANDS+=(${dir}.bin)
+    cd "$dir"
+    echo
+    echo "Benchmarking $dir"
+    echo "======================"
+    # Compile mojo first
+    $COMPILE
+    # Run Benchmark with all the commands
+    $BENCH "${COMMANDS[@]}"
+    cd ..
+  fi
 done
